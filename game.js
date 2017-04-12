4084a0942606bc6a5e291cf445da1d72db01d867
rutger muller <Rutger@rutgers-MacBook-Pro.local>
2017-04-12 20:22:44 +0200

    Set master to hard

--- game.js
+++ game.js
@@ -708,7 +708,7 @@ Asteroid = function () {
   this.collidesWith = ["ship", "bullet", "bigalien", "alienbullet"];
 
   this.breakIntoFragments = function () {
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 5; i++) {
       var roid = $.extend(true, {}, this);
       roid.vel.x = Math.random() * 6 - 3;
       roid.vel.y = Math.random() * 6 - 3;
